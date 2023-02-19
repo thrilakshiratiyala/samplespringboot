7074b9b174d9c2ce750a4f78a40c3d47df0bee71
yanusha <yanushathrilakshi89@gmail.com>
2023-02-19 23:05:08 +1100

    initial commit

---
+++ testrun-app/src/test/java/com/example/testrun/TestrunAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testrun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestrunAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
